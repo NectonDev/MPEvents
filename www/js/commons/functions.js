edada4a0cdb6c9a2e57b2aaceb96f272e8d1ce7c
Sergio Alonso <sergio.ava@gmail.com>
2015-08-14 04:40:49 +0200

    Cambios en vistas

--- www/js/commons/functions.js
+++ www/js/commons/functions.js
@@ -19,11 +19,11 @@ var functions = (function () {
                 var srcImgRuta = $(value).find("img").attr("src");
                 var titleRuta = $(value).find(".tit_evento_listado").text();
                 var dateRuta = $($(value).find(".tit_rojo")[0]).text();
-                var totalPlazas = $($(value).find(".tit_rojo")[1]).text();
-                var totalPlazasLibres = $($(value).find(".tit_rojo")[2]).text();
+                var totalPlazasOcupadas = $($(value).find(".tit_rojo")[1]).text();
+                var totalPlazas = $($(value).find(".tit_rojo")[2]).text();
                 var parrafoRuta = $(value).hasClass("parrafoclaro");
                 if (srcImgRuta != undefined) {
-                    event.img = srcImgRuta.trim();
+                    event.img = commons.madridpatinaDomain()+srcImgRuta.trim();
                 }
                 if (titleRuta != "") {
                     event.title = titleRuta.trim();
@@ -31,11 +31,12 @@ var functions = (function () {
                 if (dateRuta != "") {
                     event.date = dateRuta.trim();
                     event.totalPlazas = parseInt(totalPlazas, 10);
-                    if (isNaN(totalPlazasLibres)) {
-                        event.totalPlazasLibres = 0;
+                    if (isNaN(totalPlazasOcupadas)) {
+                        event.totalPlazasOcupadas = 0;
                     } else {
-                        event.totalPlazasLibres = parseInt(totalPlazasLibres, 10);
+                        event.totalPlazasOcupadas = parseInt(totalPlazasOcupadas, 10);
                     }
+                    event.totalPlazasLibres = event.totalPlazas - event.totalPlazasOcupadas;
                 }
                 if (parrafoRuta) {
                     event.desc = $(value).find("div").text().trim();
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('mpevents.controllers', [])
 
 .controller('HomeCtrl', function($scope, $http) {})
 
-.controller('EventsCtrl', function($scope, $http, Events) {
+.controller('EventsCtrl', function($scope, $http, $ionicLoading, Events) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -10,10 +10,22 @@ angular.module('mpevents.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function() {});
   var jsonEvents = Events.getJsonEvents();
+    $ionicLoading.show({
+        template: 'Cargando eventos :)'
+    });
     jsonEvents.then(function(data){
         $scope.events = data;
-        console.log($scope.events);
+        $ionicLoading.hide();
     });
+    $scope.doRefresh = function(){
+        var jsonEventsRefreshed = Events.getJsonEvents();
+        jsonEventsRefreshed.then(function(data){
+            $scope.events = data;
+        })
+        .finally(function() {
+            $scope.$broadcast('scroll.refreshComplete');
+        });
+    };
 })
 
 .controller('EventDetailCtrl', function($scope, $stateParams, API) {

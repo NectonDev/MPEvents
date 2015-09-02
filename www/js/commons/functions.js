407a975264df91e0e68c3226dcd89e6437379189
Sergio Alonso <sergio.ava@gmail.com>
2015-09-02 20:34:58 +0200

    Reestructuracion de API y vista de eventos

--- www/js/commons/functions.js
+++ www/js/commons/functions.js
@@ -1,6 +1,7 @@
 var functions = (function () {
     var functions = {};
     var urlEvents = commons.urlEvents();
+
     functions.getEventsByMonth = function (data, month) {
         var regExp = "\\b" + month.replace(" ", "\\b \\b") + "\\b";
         var events = [];
@@ -13,14 +14,14 @@ var functions = (function () {
         return events;
     },
     functions.getInfoOfTheEvent = function (data) {
-        var event = [];
+        var event = {};
         data.children("tr").each(function (index, value) {
             $(value).children("td").each(function (index, value) {
                 var srcImgRuta = $(value).find("img").attr("src");
                 var titleRuta = $(value).find(".tit_evento_listado").text();
                 var dateRuta = $($(value).find(".tit_rojo")[0]).text();
                 var totalPlazasOcupadas = $($(value).find(".tit_rojo")[1]).text();
-                var totalPlazas = $($(value).find(".tit_rojo")[2]).text();
+                var totalPlazasLibres = $($(value).find(".tit_rojo")[2]).text();
                 var parrafoRuta = $(value).hasClass("parrafoclaro");
                 if (srcImgRuta != undefined) {
                     var imgSrcRutaTrimmed = srcImgRuta.trim();
@@ -32,13 +33,13 @@ var functions = (function () {
                 }
                 if (dateRuta != "") {
                     event.date = dateRuta.trim();
-                    event.totalPlazas = parseInt(totalPlazas, 10);
+                    event.totalPlazasLibres = parseInt(totalPlazasLibres, 10);
                     if (isNaN(totalPlazasOcupadas)) {
                         event.totalPlazasOcupadas = 0;
                     } else {
                         event.totalPlazasOcupadas = parseInt(totalPlazasOcupadas, 10);
                     }
-                    event.totalPlazasLibres = event.totalPlazas - event.totalPlazasOcupadas;
+                    event.totalPlazas = event.totalPlazasLibres + event.totalPlazasOcupadas;
                 }
                 if (parrafoRuta) {
                     event.desc = $(value).find("div").text().trim();
--- www/js/services/api.js
+++ www/js/services/api.js
@@ -4,15 +4,15 @@ angular.module('mpevents.api', [])
 
         return {
             jsonEvents : function (data) {
-                var listEventsJson = [];
+                var listEventsJson = {};
                 var monthsWithEvents = $(data).find("span.titulo_blanco");
                 monthsWithEvents.each(function (index, value) {
-                    var event = [];
+                    var event = {};
                     var month = $(value).text();
                     var info = functions.getEventsByMonth(data, month);
-                    event["month"] = month;
-                    event["info"] = info;
-                    listEventsJson.push(event);
+                    //event["month"] = month;
+                    //event["info"] = info;
+                    listEventsJson[month] = info;
                 });
                 return listEventsJson;
             }
